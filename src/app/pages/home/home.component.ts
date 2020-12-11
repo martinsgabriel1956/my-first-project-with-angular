7fcae3278b1b613d96e35537beea04f9131036fc
Gabriel Martins <martinsgabriel1956@gmail.com>
2020-12-10 22:58:43 -0300

    home.component.html and home.component.css updated

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -3,13 +3,15 @@ import { Component, OnInit } from '@angular/core';
 import { AngularFirestore} from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
 
+// 09) Importa autenticação
+import { AngularFireAuth } from '@angular/fire/auth';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
   // Aula 08) Atributos do objeto
   items: Observable<any>;
 
@@ -17,6 +19,8 @@ export class HomeComponent implements OnInit {
 
     // Aula 08) Injeta dependências
     private afs: AngularFirestore,
+    // 09) Injeta autenticação
+    public auth: AngularFireAuth,
 ) {
 
     // Aula 08) Conectando ao Firestore
